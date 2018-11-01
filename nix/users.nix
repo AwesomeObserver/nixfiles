4be85d9f0c4f9f2823f27c1df29b190eead5b1fe
Eugene <me@awesomeobserver.me>
2018-11-01 14:51:46 +0300

    upd user

--- nix/users.nix
+++ nix/users.nix
@@ -2,29 +2,30 @@
 
 { 
   # Users block
-  users.extraUsers.awesome_observer = {
-    name = "awesome_observer";
-
-    group = "users";
-    
-    # Groups for user
-    extraGroups = [
-      "audio"
-      "disk"
-      "docker"
-      "networkmanager"
-      "systemd-journal"
-      "video"
-      "wheel"
-    ];
-
-    createHome = true;
-
-    home = "/home/awesome_observer";
-
-    uid = 1000;
-    
-    # Executable shell
-    shell = "/run/current-system/sw/bin/zsh";
+  users = {
+    extraUsers.awesome_observer = {
+      name = "awesome_observer";
+      
+      group = "users";
+      
+      # Groups for user
+      extraGroups = [
+        "audio"
+        "disk"
+        "docker"
+        "networkmanager"
+        "systemd-journal"
+        "video"
+        "wheel"
+      ];
+      
+      createHome = true;
+      
+      home = "/home/awesome_observer";
+      
+      uid = 1000; 
+      # Executable shell
+      shell = "/run/current-system/sw/bin/zsh";
+    };
   };
 }
